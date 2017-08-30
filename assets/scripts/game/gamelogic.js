6e5d89af0cbe93a08ea94f3f4428155872714177
mdcollins80 <mdcollins80@gmail.com>
2017-08-30 16:10:07 -0400

    Add Game Files
    Add javascript files for game.  Successful beginning game logic.

---
+++ assets/scripts/game/gamelogic.js
@@ -0,0 +1,37 @@
+const gameArray = [null, null, null, null, null, null, null, null, null]
+
+const click = function (player, index) {
+  if (player === 'x') {
+    if (gameArray[index] !== null) {
+      return 'box already used'
+    } else {
+      gameArray[index] = 'x'
+      return gameArray
+    }
+  } else {
+    if (gameArray[index] !== null) {
+      return 'box already used'
+    } else {
+      gameArray[index] = 'o'
+      return gameArray
+    }
+  }
+}
+
+module.export = {
+  gameArray,
+  click
+}
+
+/*
+Winning combinations for arrays:
+
+[0,1,2]
+[3,4,5]
+[6,7,8]
+[0,3,6]
+[1,4,7]
+[2,5,8]
+[0,4,8]
+[2,4,6]
+*/
